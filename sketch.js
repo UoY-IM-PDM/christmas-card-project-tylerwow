5e4d4bbe464609c98f64cec3ecec266637844c6b
tylerwow <tylerjoshuaevans2005@gmail.com>
2023-11-29 12:53:25 +0000

    Core functionality complete

--- sketch.js
+++ sketch.js
@@ -1,34 +1,74 @@
 class Player {
     x;
+    y;
 
     constructor() {
         this.x = width / 2
+        this.y = height / 2 + 125
     }
 
     draw() {
-        rectMode(CENTER);
-        rect(width / 2, height / 2 + 125, 50, 75)
+        rectMode(CORNER);
+        rect(this.x, this.y, 50, 75)
     }
 
     move() {
-        
+        if (keyIsPressed) {
+            if (key === "a") {
+                this.x -= 3;
+            }
+            if (key === "d") {
+                this.x += 3;
+            }
+        }
+    }
+}
+
+class Gift {
+    x;
+    y;
+    isCollected;
+    isMissed;
+
+    constructor() {
+        this.x = random(0, width - 40);
+        this.y = 0;
+        this.isCollected = false;
+        this.isMissed = false;
+    }
+
+    draw() {
+        if(!this.isCollected) {
+            rectMode(CORNER);
+            rect(this.x, this.y, 50, 50);
+
+            this.y++;
+        }
+    }
+
+    checkCollection(playerX, playerY) {
+        if (this.x + 15 > playerX && this.x - 15 < playerX && this.y + 50 > playerY ) {
+            this.isCollected = true;
+        }
     }
 }
 
 let player;
+let gift;
 
 function setup() {
     createCanvas(500, 500);
 
     player = new Player();
+    gift = new Gift();
 }
 
 function draw() {
     background(255);
 
     player.draw();
-}
+    player.move();
 
-function keyPressed() {
-    
+    gift.draw();
+    gift.checkCollection(player.x, player.y);
 }
